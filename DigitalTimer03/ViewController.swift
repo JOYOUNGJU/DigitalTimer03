3c3da346ba8f780f025b28efdf0b9ee6034fe8c2
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-10 15:09:32 +0900

    TimeSwitch

--- DigitalTimer03/ViewController.swift
+++ DigitalTimer03/ViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var timerSwitch: UISwitch!
+    
     var myTimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
-            self.updateTime()
-        })
+        //myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            //self.updateTime()
+        //})
+        timerSwitch.setOn(false, animated: true)
     }
 
     func updateTime() {
@@ -29,7 +33,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func TiSw(_ sender: Any) {
+        if timerSwitch.isOn == true {
+            //            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
 }
-
